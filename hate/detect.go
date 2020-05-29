f27a4f2ef7b61c11718eaece4c28f114120a82a5
GabielFemi <gabrielfemi799@gmail.com>
2020-05-29 17:59:58 +0200

    add function to check if the speech is hate word
    Took 2 minutes

--- hate/detect.go
+++ hate/detect.go
@@ -1,7 +1,17 @@
 package hateSpeech
 
-import "net/http"
+import (
+	"github.com/gorilla/mux"
+	"net/http"
+)
 
 func Detect(w http.ResponseWriter, r *http.Request) {
+	render(w, "index.html", r)
+	vars := mux.Vars(r)
+	speechGottenFromUser := vars["speech"]
+	isSpeechHateWord(speechGottenFromUser)
+}
+
+func isSpeechHateWord(speech string) {
 
 }
