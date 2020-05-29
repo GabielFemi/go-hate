32840f7631f4581d8535bca5a1953a056914055e
GabielFemi <gabrielfemi799@gmail.com>
2020-05-29 18:06:47 +0200

    check if HTTP  request is a POST request
    Took 43 seconds

--- hate/detect.go
+++ hate/detect.go
@@ -7,9 +7,12 @@ import (
 
 func Detect(w http.ResponseWriter, r *http.Request) {
 	render(w, "index.html", r)
-	vars := mux.Vars(r)
-	speechGottenFromUser := vars["speech"]
-	isSpeechHateWord(speechGottenFromUser)
+	if r.Method == "POST" {
+		vars := mux.Vars(r)
+		speechGottenFromUser := vars["speech"]
+		isSpeechHateWord(speechGottenFromUser)
+	}
+
 }
 
 // Loop through the sentence, if buhari(for example) is found, check if the current word is the hashmap
