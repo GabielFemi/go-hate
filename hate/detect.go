8226f6b57eaba18b27b7867f734248b33c27d8a5
GabielFemi <gabrielfemi799@gmail.com>
2020-05-29 18:01:57 +0200

    add comment to code
    Took 2 minutes

--- hate/detect.go
+++ hate/detect.go
@@ -12,6 +12,11 @@ func Detect(w http.ResponseWriter, r *http.Request) {
 	isSpeechHateWord(speechGottenFromUser)
 }
 
+// Loop through the sentence, if buhari(for example) is found, check if the current word is the hashmap
+// containing the hate words
+// if true, return true
+// otherwise, return false
+
 func isSpeechHateWord(speech string) {
 
 }
